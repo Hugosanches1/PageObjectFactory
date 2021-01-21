4c2e5149cb422c1f51b23e147eed7dc688d9baf9
Hugosanches <Hugosanches87@gmail.com>
2021-01-21 00:20:39 -0300

    Adicionando FindBys FindAll

--- PageObject/src/test/java/br/com/projeto/page/PageObjects.java
+++ PageObject/src/test/java/br/com/projeto/page/PageObjects.java
@@ -1,8 +1,12 @@
 package br.com.projeto.page;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 
 public class PageObjects {
@@ -17,6 +21,27 @@ public class PageObjects {
 	
 	@FindBy(xpath="//button[@name='login']")
 	private WebElement butEntrar;
+	
+	@FindBys({
+		@FindBy(id="email"),
+		@FindBy(name="pass")
+	})
+	private List<WebElement> lista;
+
+	@FindAll({
+		@FindBy(id="email"),
+		@FindBy(name="pass"),
+		@FindBy(xpath="//button[@name='login']")
+	})
+	private List<WebElement> listaAll;
+	
+	public List<WebElement> getLista() {
+		return lista;
+	}
+
+	public List<WebElement> getListaAll() {
+		return listaAll;
+	}
 
 	public PageObjects(WebDriver driver) {
 		this.driver = driver;
--- PageObject/src/test/java/br/com/projeto/page/Testes.java
+++ PageObject/src/test/java/br/com/projeto/page/Testes.java
@@ -8,7 +8,7 @@ public class Testes {
 	static WebDriver driver;
 
 	public static void main(String[] args) {
-		testeFactory();
+		testeFindBy_FindAll();
 	}
 	
 	static void testeFactory() {
@@ -24,5 +24,27 @@ public class Testes {
 		obj.getEmail().sendKeys("hugosanches1@hotmail.com");
 		obj.getSenha().sendKeys("helaynn");
 		obj.getButEntrar().click();
+		
+	}
+	static void testeFindBy_FindAll() {
+		
+		System.setProperty("webdriver.gecko.driver", "C:/Devtools/Drivers/geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com");
+		
+		driver.manage().window().maximize();
+		
+		PageObjects obj = new PageObjects(driver);
+
+		System.out.println(obj.getLista().size());
+		System.out.println(obj.getLista());
+		
+		System.out.println(obj.getListaAll().size());
+		System.out.println(obj.getListaAll());
+		
+		obj.getListaAll().get(0).sendKeys("hugosanches1@hotmail.com");
+		obj.getListaAll().get(1).sendKeys("helaynn");
+		obj.getListaAll().get(2).click();
+		
 	}
 }
